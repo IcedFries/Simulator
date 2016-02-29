103929a718e2bac80cf262eb61e3200bce6d4194
nicklas669 <nicklas669@gmail.com>
2016-02-29 14:38:09 +0100

    Assignment 4, step 3 almost done. I added the simulator dependency and updated the getTransition(). Haven't been able to fire a transition though, nothing happens...

--- src/dk/dtu/compute/se/mdsu/tutorial1/simulator/SimulatorCommandHandler.java
+++ src/dk/dtu/compute/se/mdsu/tutorial1/simulator/SimulatorCommandHandler.java
@@ -9,11 +9,12 @@ import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.gmf.runtime.notation.View;
+
 import petrinet.Arc;
 import petrinet.Node;
-import petrinet.PetrinetFactory;
 import petrinet.Place;
-import petrinet.Token;
 import petrinet.Transition;
 
 public class SimulatorCommandHandler extends AbstractHandler {
@@ -32,7 +33,25 @@ public class SimulatorCommandHandler extends AbstractHandler {
 		Transition transition = getTransition(context);
 		setBaseEnabled(isEnabled(transition));
 	}
-	
+
+	// static private Transition getTransition(Object context) {
+	// if (context instanceof IEvaluationContext) {
+	// IEvaluationContext evaluationContext = (IEvaluationContext) context;
+	// Object object = evaluationContext.getDefaultVariable();
+	// if (object instanceof List) {
+	// @SuppressWarnings("rawtypes")
+	// List list = (List) object;
+	// if (list.size() == 1) {
+	// object = list.get(0);
+	// if (object instanceof Transition) {
+	// return (Transition) object;
+	// }
+	// }
+	// }
+	// }
+	// return null;
+	// }
+
 	static private Transition getTransition(Object context) {
 		if (context instanceof IEvaluationContext) {
 			IEvaluationContext evaluationContext = (IEvaluationContext) context;
@@ -44,23 +63,33 @@ public class SimulatorCommandHandler extends AbstractHandler {
 					object = list.get(0);
 					if (object instanceof Transition) {
 						return (Transition) object;
+					} else if (object instanceof IGraphicalEditPart) {
+						IGraphicalEditPart editPart = (IGraphicalEditPart) object;
+						Object model = editPart.getModel();
+						if (model instanceof View) {
+							Object element = ((View) model).getElement();
+							if (element instanceof Transition) {
+								return (Transition) element;
+							}
+						}
 					}
 				}
 			}
 		}
 		return null;
 	}
+
 	/**
-	 * compute whether there are enough tokens on
-		each input place of a transition; Note that
-		there could be more than one arc between the
-	 	same place and the same transition (
+	 * compute whether there are enough tokens on each input place of a
+	 * transition; Note that there could be more than one arc between the same
+	 * place and the same transition (
+	 * 
 	 * @param transition
 	 * @return
 	 */
 	static private boolean isEnabled(Transition transition) {
-		
-		for (Arc arc: transition.getIn()) {			
+
+		for (Arc arc : transition.getIn()) {
 			Node node = arc.getSource();
 			if (node instanceof Place) {
 				Place place = (Place) node;
@@ -73,46 +102,45 @@ public class SimulatorCommandHandler extends AbstractHandler {
 	}
 
 	/**
-	 * code which fires a transition, i.e.
-		removes one token for each incoming arc from
-		the target place and adds one token to the
-		target place of each outgoing arc.
+	 * code which fires a transition, i.e. removes one token for each incoming
+	 * arc from the target place and adds one token to the target place of each
+	 * outgoing arc.
+	 * 
 	 * @param transition
 	 */
 	static private void fire(Transition transition) {
-		
+
 		/**
-		 * Instead of changing the tokens programmatically now, 
-		 * in the command handler from Assignment 1,
-		 *  you can create a FireTransitionCommand 
-		 *  and execute it in the respective editing domain by a piece of code,
-		 *   that looks as follows:
-  			EditingDomain domain = AdapterFactoryEditingDomain.getEditingDomainFor(transition);
-  			if (domain != null)
-    		domain.getCommandStack().execute( new FireTransitionCommand(domain, transition));
+		 * Instead of changing the tokens programmatically now, in the command
+		 * handler from Assignment 1, you can create a FireTransitionCommand and
+		 * execute it in the respective editing domain by a piece of code, that
+		 * looks as follows: EditingDomain domain =
+		 * AdapterFactoryEditingDomain.getEditingDomainFor(transition); if
+		 * (domain != null) domain.getCommandStack().execute( new
+		 * FireTransitionCommand(domain, transition));
 		 */
-		
-		  EditingDomain domain = AdapterFactoryEditingDomain.getEditingDomainFor(transition);
-		  if (domain != null)
-		    domain.getCommandStack().execute( new FireTransitionCommand(domain, transition));
-		
-		  //Nedenstående kode er flyttet til FireTransitionCommand nu
-//		for (Arc arc: transition.getIn()) {			
-//			Node node = arc.getSource();
-//			if (node instanceof Place) {
-//				Place place = (Place) node;
-//				if (!place.getTokens().isEmpty()) {
-//					place.getTokens().remove(0);
-//				}
-//			}
-//		}		
-//			for (Arc arc: transition.getOut()) {			
-//			Node node = arc.getTarget();
-//			if (node instanceof Place) {
-//				Token token = PetrinetFactory.eINSTANCE.createToken();
-//				Place place = (Place) node;
-//				place.getTokens().add(token);
-//			}
-//		}		
+
+		EditingDomain domain = AdapterFactoryEditingDomain.getEditingDomainFor(transition);
+		if (domain != null)
+			domain.getCommandStack().execute(new FireTransitionCommand(domain, transition));
+
+		// Nedenstående kode er flyttet til FireTransitionCommand nu
+		// for (Arc arc: transition.getIn()) {
+		// Node node = arc.getSource();
+		// if (node instanceof Place) {
+		// Place place = (Place) node;
+		// if (!place.getTokens().isEmpty()) {
+		// place.getTokens().remove(0);
+		// }
+		// }
+		// }
+		// for (Arc arc: transition.getOut()) {
+		// Node node = arc.getTarget();
+		// if (node instanceof Place) {
+		// Token token = PetrinetFactory.eINSTANCE.createToken();
+		// Place place = (Place) node;
+		// place.getTokens().add(token);
+		// }
+		// }
 	}
 }
